0fe46da00f3aac402a41958564f29acd44d7f739
Mihai Dogaru <dogarumihai2537@gmail.com>
2025-10-02 10:38:56 +0300

    Add inputLength slider styling

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 console.log("Hello world!");
+
+const inputLengthEl = document.getElementById("inputLength");
+const inputLengthResultEl = document.getElementById("inputLengthResult");
+
+console.log(inputLengthEl, inputLengthResultEl);
+
+inputLengthResultEl.textContent = inputLengthEl.value;
+
+inputLengthEl.addEventListener("input", (ev) => {
+  inputLengthResultEl.textContent = inputLengthEl.value;
+
+  let minLimit = inputLengthEl.min;
+  let maxLimit = inputLengthEl.max;
+  console.log(minLimit);
+  let x = inputLengthEl.value - minLimit;
+
+  let percent = (x * 100.0) / (maxLimit - minLimit);
+  console.log(`${percent}%`);
+
+  inputLengthEl.style.setProperty("--range-percentage", `${percent}%`);
+});
